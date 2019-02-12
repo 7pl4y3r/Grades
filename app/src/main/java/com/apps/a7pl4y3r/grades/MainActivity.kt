ab19ae24d9f676da3891590a320695eee7a72cab
7pl4y3r <7pl4y3r@gmail.com>
2019-02-12 21:49:45 +0200

    initial commit

---
+++ app/src/main/java/com/apps/a7pl4y3r/grades/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.apps.a7pl4y3r.grades
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
